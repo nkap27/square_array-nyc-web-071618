f97349d740b7d2f3db7df8abac8313042efff164
Natasha K <kapr.natasha@gmail.com>
2018-07-15 18:24:34 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
 def square_array(array)
   array_squared = []
   array.each do |number|
-    array.push(number ^ 2)
-  end
-  return array_squared
+    array.push(number ^ 2)return array_squared
 end
